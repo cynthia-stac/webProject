70719b0ca96ae5690683ca2c8c756877f552770d
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-10-01 11:20:20 +0200

    Added some configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ["./index.html","./src**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+
+    extend: {
+      boxShadow: {
+        'cyn': "0 0 10px 10px rgb(0,100,200,0.35)"
+      }
+    },
   },
   plugins: [],
 }
