5d0640a47dbeff516269cb9420804c576f06d566
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-10-01 11:18:39 +0200

    Added javascript

--- src/main.js
+++ src/main.js
@@ -1,24 +1,61 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+//humber menus
+let open = document.getElementById('open')
+let close =document.getElementById('close')
+let menu = document.getElementById('menu')
+open.addEventListener('click',()=>{
+    close.classList.toggle('hidden');
+    menu.classList.toggle('hidden')
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+})
+close.addEventListener('click',()=>{
+    close.classList.toggle('hidden');
+    menu.classList.toggle('hidden');
+})
+//submit button for feedback
+let button = document.getElementById('submit')
+function ReplyFeedback(){
+    let name = document.getElementById('name').value
+    let email = document.getElementById('email').value
+    let feedback = document.getElementById('feedback').value
+    let response = document.getElementById('response');
+    if(name&&email&&feedback){
+    response.innerHTML = `
+    <div class="bg-green-500 relative rounded-lg dark:bg-blue-500 dark:text-slate-200">
+    <h1 class="text-2xl font-bold text-red-600  pl-2 md:p-2">Thank you for giving us a feedback</h1>
+    <p class="pl-2 md:p-2">Dear <span class="text-blue-950 italic"><em>${name}</em></span></p> 
+     <p class="pl-2 md:p-2">Email address <span class="text-blue-950 italic"><em>${email}</em></span></p> 
+    <p class="pl-2 md:p-2">Your feeback <span class="text-blue-950 italic">${feedback}</span></p>
+    <p class="pl-2 md:p-2">Is warmly well received and we thank you for your participation to our tearm building</p>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    ` 
+    button.innerHTML = '<h1 class="text-white text-2xl">Remove</h1>'
+    button.removeEventListener('click',ReplyFeedback)
+    button.addEventListener('click',remove)
+    }
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('feedback').value = '';
+    
+}
+button.addEventListener('click',ReplyFeedback)
+function remove(){
+    let response = document.getElementById('response');
+    button.innerHTML = '<h1 class="text-white text-2xl">Submit</h1>'
+    response.innerHTML = ''
+    button.removeEventListener('click',remove)
+    button.addEventListener('click',ReplyFeedback)
 
-setupCounter(document.querySelector('#counter'))
+}  
+//dark mode
+let dark = document.getElementById('dark');
+let phoneDark = document.getElementById('fd')
+function changeMode(){
+    const element = document.querySelector('html').classList
+    element.toggle('dark')
+    console.log(element)
+}
+dark.addEventListener('click',changeMode)
+phoneDark.addEventListener('click',changeMode)
+
+
